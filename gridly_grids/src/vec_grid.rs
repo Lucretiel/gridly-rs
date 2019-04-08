cd930b79ec26bdb5b7035ca503ecaff47b6d1023
Nathan West <Lucretiel@gmail.com>
2019-04-08 02:59:11 -0400

    Added comments

--- gridly_grids/src/vec_grid.rs
+++ gridly_grids/src/vec_grid.rs
@@ -16,7 +16,7 @@ impl<T> VecGrid<T> {
     /// This function returns the dimensions and vector because it doesn't do any
     /// bounds checking on the vector after make_storage, which means that the returned
     /// vector is not guaranteed to obey the invariants of the Grid
-    fn build(
+    fn prepare(
         dimensions: impl Into<Vector>,
         make_storage: impl FnOnce(usize, &Vector, &mut Vec<T>),
     ) -> Option<(Vector, Vec<T>)> {
@@ -36,8 +36,9 @@ impl<T> VecGrid<T> {
         }
     }
 
+    /// Create a new `VecGrid`, filled with elements by repeatedly calling a function
     pub fn new_fill_with(dimensions: impl Into<Vector>, gen: impl FnMut() -> T) -> Option<Self> {
-        Self::build(dimensions, move |volume, _dim, storage| {
+        Self::prepare(dimensions, move |volume, _dim, storage| {
             storage.extend(repeat_with(gen).take(volume));
         })
         .map(|(dimensions, storage)| Self {
@@ -46,38 +47,44 @@ impl<T> VecGrid<T> {
         })
     }
 
+    /// Create a new `VecGrid` by calling a function for each location in the
+    /// grid
     pub fn new_with(
         dimensions: impl Into<Vector>,
         mut gen: impl FnMut(&Location) -> T,
     ) -> Option<Self> {
-        Self::build(dimensions, move |_volume, dimensions, storage| {
+        Self::prepare(dimensions, move |_volume, dimensions, storage| {
             storage.extend(
                 RowRange::range(dimensions.rows)
                     .flat_map(move |row| {
                         ColumnRange::range(dimensions.columns).map(move |column| row + column)
                     })
                     .map(move |location| gen(&location)),
-            )
+            );
         })
         .map(|(dimensions, storage)| Self {
             dimensions,
             storage,
         })
     }
+
     /// Given a bounds-checked location, return the index of the vector
-    /// associated with that location
-    fn index_for_location(&self, loc: &Location) -> usize {
+    /// associated with that location. Performs no bounds checking, either
+    /// on the input `location` or the output `usize`
+    unsafe fn index_for_location(&self, loc: &Location) -> usize {
         (loc.row.0 as usize * self.dimensions.columns.0 as usize) + loc.column.0 as usize
     }
 }
 
 impl<T: Default> VecGrid<T> {
+    /// Create a new `VecGrid` filled with the default value of `T` in each cell
     pub fn new(dimensions: impl Into<Vector>) -> Option<Self> {
         Self::new_fill_with(dimensions, Default::default)
     }
 }
 
 impl<T: Clone> VecGrid<T> {
+    /// Create a new `VecGrid` filled with clones of `value`
     pub fn new_fill(dimensions: impl Into<Vector>, value: &T) -> Option<Self> {
         Self::new_fill_with(dimensions, move || value.clone())
     }
